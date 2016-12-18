06066754028f4b26183b9155635465c6a4bfce32
MaksatRysbekov <max.rysbekov96@mail.ru>
2016-12-18 21:13:27 +0300

    veraenderungen

--- main.cpp
+++ main.cpp
@@ -17,10 +17,41 @@ int main() {
     for (int x = 0; x < 256; x++)   normal [x] = 0;
     for (int x = 0; x < img.rows; x++)  for(int y = 0; y < img.cols; y++)   normal[(img.at<uchar>(x,y))]+=1;
     
-    for(int i=0; x<256; x++){
-     normal[x]/=(img.rows*img.cols);
-     q[x] = round((255*normal[x]));
-     cout<<x<<"-->"<<normal[x]<<"s-->"<<q[x]<<endI;
+       for (int x = 0; x < 256; x++){
+        normal [x]/=(img.cols*img.rows);
+        q[x] = round((255*normal [x]));
+//        cout<<x<<"-->"<<normal [x]<<"  s --> "<<q[x]<<endl;
+    }
+
+    for (int x = 0; x < 256; x++){
+        if (x <= 28){
+            user[x] = 0.05;
+            pix[x] = 28;
+        }else if (x>28 && x <= 56){
+            user[x] = 0.1;
+            pix[x] = 56;
+        }else if (x>56 && x <= 84){
+            user[x] = 1.0;
+            pix[x] = 86;
+        }else if (x>86 && x <= 112){
+            user[x] = 0.8;
+            pix[x] = 112;
+        }else if (x>112 && x <= 140){
+            user[x] = 0.75;
+            pix[x] = 140;
+        }else if (x>140 && x <= 168){
+            user[x] = 0.4;
+            pix[x] = 168;
+        }else if (x>168 && x <= 196){
+            user[x] = 0.03;
+            pix[x] = 196;
+        }else if (x>196&& x <= 224){
+            user[x] = 0.05;
+            pix[x] = 224;
+        }else if (x>224 && x <= 255){
+            user[x] = 0.02;
+            pix[x] = 255;
+        }
     }
     return 0;
 }
